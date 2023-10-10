3f6fbf8b4c2ef92149034e5038f8edfd810f26b9
Guilherme Amaral <94094527+guiopen@users.noreply.github.com>
2023-10-10 02:56:05 -0300

    criado método de verificar cheque mate, deve ser usado apenas se cheque for verdadeiro

--- main_test.py
+++ main_test.py
@@ -1,6 +1,5 @@
 from classes.controles.controle_jogo import ControleJogo
 
 jogo = ControleJogo()
-jogo.tabuleiro[0][0].posicao = [1,2]
-jogo.sincronizar_posicoes_tabuleiro()
-print(jogo.tabuleiro[0][0].posicao)
+jogo.tabuleiro[0][3].posicao = [6,4]
+print(jogo.verifica_cheque())
